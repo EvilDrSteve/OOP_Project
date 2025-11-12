cd3a68e2bf70dd7fae8203eb46d9b9263b26da3a
evildrsteve <2001.shehryar@gmail.com>
2025-11-12 22:53:07 +0500

    Game class initial

---
+++ Game.cpp
@@ -0,0 +1,24 @@
+#include "Game.hpp"
+#include <SFML/Window.hpp>
+Game::Game(){
+
+}
+Game::~Game(){
+
+}
+
+void Game::init(const char* windowTitle, int width, int height, bool fullScreen){
+
+}
+
+void Game::update(){
+
+}
+
+void Game::render(){
+
+}
+
+void Game::handleEvents(){
+
+}
---
+++ main.cpp
@@ -0,0 +1,23 @@
+
+
+
+
+
+
+
+
+class Game {
+
+    public:
+
+        Game();
+        ~Game();
+        
+        void init(const char* windowTitle, int width, int height, bool fullScreen);
+        void update();
+        void render();
+        void handleEvents();
+
+    private:
+        bool isRunning;
+};
