c58b6dcf0036c1c7510dc826c1fe0b733265b00c
EvilDrSteve <2001.shehryar@gmail.com>
2025-11-13 20:12:19 +0500

    updated Game module

---
+++ include/Game.hpp
@@ -0,0 +1,36 @@
+#ifndef GAME_H
+#define GAME_H
+
+#include "GameState.hpp"
+
+class Game {
+
+    
+    private:
+            bool isRunning;
+            double dt;
+    
+            sf::RenderWindow* window;
+            sf::Event event;
+            sf::Clock clock;
+    
+            std::stack<State*> states;
+    
+            void initStates();
+    public:
+
+        Game();
+        ~Game();
+        
+        void init();
+        void update();
+        void render();
+        void handleEvents();
+        void run();
+        bool getRunning();
+
+        void updateClock();
+
+};
+
+#endif
---
+++ src/Game.cpp
@@ -0,0 +1,69 @@
+#include "Game.hpp"
+
+
+Game::Game() {
+    this->init();
+    this->initStates();
+}
+Game::~Game() {
+    std::cout << "Ending Application" << std::endl;
+
+    delete this->window;
+
+    while(!this->states.empty()){
+        delete this->states.top();
+        this->states.pop();
+    }
+
+}
+
+void Game::init() {
+    this->isRunning = true;
+
+    this->window = new sf::RenderWindow(sf::VideoMode(800, 600, 32), "Diner Dash", sf::Style::Default | sf::Style::Default);
+    this->window->setFramerateLimit(60);
+    this->window->setVerticalSyncEnabled(true);
+
+}
+
+void Game::run() {
+    while(this->getRunning()){
+        this->updateClock();
+        this->update();
+        this->render();
+    }
+}
+void Game::update() {
+    this->handleEvents();
+
+    if(!this->states.empty()){
+        this->states.top()->update(this->dt);
+        if(!this->states.top()->getActive()){
+            delete this->states.top();
+            this->states.pop();
+        }
+        
+    }else this->isRunning = false;
+
+}
+
+void Game::updateClock(){
+    this->dt = this->clock.restart().asSeconds();
+}
+void Game::render() {
+   
+    this->window->clear();
+
+    if(!this->states.empty()) this->states.top()->render();
+    this->window->display();
+}
+
+void Game::handleEvents() {
+
+}
+
+bool Game::getRunning() { return this->isRunning; }
+
+void Game::initStates(){
+    this->states.push(new GameState(this->window));
+}
