85d5ad2f3acb39aa7471382500cd1baa956a043d
EvilDrSteve <2001.shehryar@gmail.com>
2025-11-20 00:08:38 +0500

    Added path following

--- include/Player.hpp
+++ include/Player.hpp
@@ -1,17 +1,24 @@
 #ifndef PLAYER_H
 #define PLAYER_H
 #include "Character.hpp"
+#include <vector>
 
 class Player : public Character {
-    private:
-        float movementSpeed;
-    public:
-        Player(int x, int y);
-        ~Player();
+private:
+    float movementSpeed;
+    std::vector<sf::Vector2f> currentPath;
+    int currentStep;
+    
+public:
+    Player(int x, int y, int gridSize = 16);
+    ~Player();
 
-        virtual void move(const float& dt, const float x, const float y) override;
-        virtual void update(const float& dt) override;
+    virtual void move(const float& dt, const float x, const float y) override;
+    virtual void update(const float& dt) override;
+    
+    void setPath(const std::vector<sf::Vector2f>& path);
+    bool isMoving() const;
+
 };
-
 
 #endif
--- src/Player.cpp
+++ src/Player.cpp
@@ -1,18 +1,57 @@
 #include "Player.hpp"
+#include <cmath>
 
-Player::Player(int x, int y) : Character(x, y, 16, 32){
-    this->movementSpeed = 100;
+Player::Player(int x, int y, int gridSize){
+    this->shape.setOrigin(gridSize/2, gridSize + gridSize / 2);  // (width/2, height)
+
+    this->movementSpeed = 300.f; // pixels per second
+    this->currentStep = 0;
+
+    float pixelX = x * gridSize + gridSize / 2.0f;
+    float pixelY = y * gridSize + gridSize / 2.0f;
+
+    this->shape.setSize(sf::Vector2f(16, 32));
+    this->shape.setScale(sf::Vector2f(3, 3));
+    this->shape.setPosition(pixelX, pixelY);
+
+    this->shape.setFillColor(sf::Color::Green);
 }
 
-Player::~Player(){
+Player::~Player() {}
 
+void Player::move(const float& dt, const float x, const float y) {
+    this->shape.move(x * this->movementSpeed * dt, y * this->movementSpeed * dt);
 }
 
-void Player::move(const float& dt, const float dirX, const float dirY){
-    this->shape.move(dirX * this->movementSpeed * dt, dirY * this->movementSpeed * dt);
+void Player::setPath(const std::vector<sf::Vector2f>& path) {
+    this->currentPath = path;
+    this->currentStep = 0;
 }
 
-void Player::update(const float& dt){
-
+bool Player::isMoving() const {
+    return this->currentStep < this->currentPath.size();
 }
 
+void Player::update(const float& dt) {
+    if (this->currentStep < this->currentPath.size()) {
+        sf::Vector2f pos = this->shape.getPosition();
+        sf::Vector2f target = this->currentPath[currentStep];
+
+        sf::Vector2f diff = (target) - (pos);
+
+        float distanceToTarget = std::sqrt(diff.x * diff.x + diff.y * diff.y);
+        
+
+        float moveDistance = movementSpeed * dt;
+        if(moveDistance >= distanceToTarget){
+            this->shape.setPosition(target);
+            currentStep++;
+        }
+        else if (distanceToTarget > 1.0f) {
+            // Move towards target
+            float moveX = (diff.x / distanceToTarget) * movementSpeed * dt;
+            float moveY = (diff.y / distanceToTarget) * movementSpeed * dt;
+            this->shape.move(moveX, moveY);
+        }
+    }
+}
